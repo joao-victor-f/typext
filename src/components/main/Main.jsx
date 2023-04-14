912b235bfc018ada442376a1e9f3eb3c7fd2f5f5
victor <joaovicif2021@gmail.com>
2023-04-13 23:21:47 -0300

    Remoção de imagem de teclado

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,18 +5,12 @@ const Main = () => {
     return (
         <main>
             <div className='main-container'>
-                <div className='main-intro-container'>
-                    <Logo/>
+                <Logo/>
 
-                    <div className='site-info'>
-                        <h4 id='site-name-info'>A aglutinação de "type text"</h4>
-                        <p id='site-subject'>Um site de testes de velocidade de digitação</p>
-                        <button className='btn-get-started'>Começar</button>
-                    </div>
-                </div>
-
-                <div className='img-container'>
-                    <img src='../public/keyboard-icon.png'></img>
+                <div className='site-info'>
+                    <h4 id='site-name-info'>A aglutinação de "type text"</h4>
+                    <p id='site-subject'>Um site de testes de velocidade de digitação</p>
+                    <button className='btn-get-started'>Começar</button>
                 </div>
             </div>
         </main>
